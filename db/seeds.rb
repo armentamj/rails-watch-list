87a24dfae2671bb5c5bfa37a0e30bfc471574da3
Joel Armenta <armentamj@gmail.com>
2024-10-24 10:40:22 +0200

    Commiting the code for this challenge.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +8,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Movie.destroy_all
+
+10.times do
+  movie = Movie.new(
+    title: Faker::Movie.title,
+    overview: Faker::Address.street_address,
+    poster_url: Faker::LoremFlickr.image,
+    rating: rand(0.1..10.0)
+  )
+  puts "Created #{movie[:title]}."
+  movie.save!
+end
